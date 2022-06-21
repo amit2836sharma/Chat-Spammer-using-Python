6bf7f9298650b686d0f62a5e001368be117f943b
amit2836sharma <95281963+amit2836sharma@users.noreply.github.com>
2022-06-21 17:16:30 +0530

    Add files via upload

---
+++ Chat Spammer.py
@@ -0,0 +1,7 @@
+import pyautogui as p
+import time
+time.sleep(4)
+
+for i in range(10):                        #in paranthesis you can write how many time you have to repeat the loop
+    p.typewrite("Add your message here")   #on this line you can add ypur message
+    p.press("enter")                       #here you need to add the key of whcih is on the keyboard
